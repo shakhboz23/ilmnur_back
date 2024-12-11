d68cbec6e821b5093e41eace1db69c5c44c893e9
shahbozhoca <upwork02092005@gmail.com>
2024-12-11 21:30:15 +0500

    .

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -3,9 +3,10 @@ import { BotService } from './bot.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Bot } from './models/bot.model';
 import { BotUpdate } from './bot.update';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Bot])],
+  imports: [SequelizeModule.forFeature([Bot]), UserModule],
   controllers: [],
   providers: [BotService, BotUpdate],
   exports: [BotService]
